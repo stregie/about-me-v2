cb3cd5640c14c237b0edd7f59aa3c0833185821f
stregie <stregie@outlook.com>
2023-05-14 22:17:06 +0200

    CSS modifications

---
+++ public/js/00_header.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $('#off-canvas-toggler-btn').click(toggleOffCanvas);
+  $('.navigation-dropdown-btn').click(toggleDropdown);
+});
+
+// Open and close navigation menu in mobile view
+function toggleOffCanvas(){
+  $('.navigation-menu').toggleClass('hide');
+  $(this).toggleClass('active');
+};
+
+// Open and close dropdown menus in mobile view
+function toggleDropdown(){      
+  $(this).nextAll(".navigation-dropdown-menu").toggleClass("open");
+  $(this).toggleClass("open");      
+};
+
+
+// Hide navigation bar when scrolling down, show when scrolling up
+let prevScrollPos = window.pageYOffset; // Position a moment ago
+let scrollStartPos = window.pageYOffset; // Position before the last 100 px long scroll
+
+window.addEventListener('scroll', (event) => {
+  let currScrollPos = window.pageYOffset;        
+  let scrollDistance = Math.abs(currScrollPos - scrollStartPos);
+  let offCanvasStatus = $('#off-canvas-toggler-btn').attr('class');
+
+  if (currScrollPos < 100 || offCanvasStatus == "active"){ // always visible when scrolled to top or if off-canvas is active
+    $('.navigation').removeClass('navigation-hide');
+  } else {
+    if (scrollDistance > 100){
+      if (prevScrollPos < currScrollPos){ // Scroll down
+        $('.navigation').addClass('navigation-hide');            
+      } else if (prevScrollPos > currScrollPos){ // Scroll up
+        $('.navigation').removeClass('navigation-hide');
+      }
+      scrollStartPos = window.pageYOffset;
+    }
+  }
+
+  prevScrollPos = window.pageYOffset;
+});
